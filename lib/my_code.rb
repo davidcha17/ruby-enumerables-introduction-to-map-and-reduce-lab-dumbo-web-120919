97eaae2d9cd8b89479d3805272d9d7c9debd724b
David Cha <david.cha44@qmail.cuny.edu>
2019-11-22 22:48:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 
 def reduce_to_all_true(source_array, starting_point = 0)
   i = 0
-  while i < source_array.length do 
+  while i < source_array.length do
     return false if !source_array[i]
     i += 1
   end
