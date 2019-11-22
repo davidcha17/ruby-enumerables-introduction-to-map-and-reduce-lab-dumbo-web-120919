b704ca1f0fd245079bc70de2886cd94a4910bee4
David Cha <david.cha44@qmail.cuny.edu>
2019-11-22 22:46:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,4 +27,14 @@ def map_to_double(source_array)
     i += 1
   end
   return new
+end
+
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(source_array[i] ** 2)
+    i += 1
+  end
+  return new
 end 
